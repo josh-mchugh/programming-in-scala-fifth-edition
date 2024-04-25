a9ebeb3aaf9cbc5a43768885dc697c02544a9bd3
Josh McHugh <joshua.mchugh01@gmail.com>
2024-04-24 22:05:40 -0400

    A simplified way to using Array

--- chapter-3/helloarray.scala
+++ chapter-3/helloarray.scala
@@ -1,8 +1,4 @@
 @main def m(arg: String*) =
-  val greetStrings = new Array[String](3)
-  greetStrings(0) = "Hello"
-  greetStrings(1) = ", "
-  greetStrings(2) = "World!"
-
+  val greetStrings = Array("Hello", ", ", "World!")
   for i <- 0 to 2 do
    print(greetStrings(i))
