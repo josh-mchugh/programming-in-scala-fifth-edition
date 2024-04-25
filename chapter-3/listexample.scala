187252b6f58e5b86c3dc889b5c5f6749a8fe8f34
Josh McHugh <joshua.mchugh01@gmail.com>
2024-04-24 23:11:01 -0400

    Example of List 'con' operation

--- chapter-3/listexample.scala
+++ chapter-3/listexample.scala
@@ -3,4 +3,11 @@
   val threeFour = List(3, 4)
   val oneTwoThreeFour = oneTwo ::: threeFour
 
+  println("Output of List concatenation:")
   println(oneTwoThreeFour)
+
+  val twoThree = List(2, 3)
+  val oneTwoThree = 1 :: twoThree
+
+  println("Output of List 'con' prepend operation:")
+  println(oneTwoThree)
