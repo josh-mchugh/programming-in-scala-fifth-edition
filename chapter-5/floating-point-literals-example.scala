5b85b9e316bb2769f962c52545ba4e3fedb11138
Josh McHugh <joshua.mchugh01@gmail.com>
2024-04-29 23:35:40 -0400

    Example of floating point literals

---
+++ chapter-5/floating-point-literals-example.scala
@@ -0,0 +1,11 @@
+@main def m(args: String*) =
+  val big = 1.2345
+  val bigger = 1.2345e1
+  val bitterStill = 123E45
+  val trillion = 1_000_000_000e3
+
+  val little = 1.2345F
+  val littleBigger = 3e5f
+
+  val anotherDouble = 3e5
+  val yetAnother = 3e5D
