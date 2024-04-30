7fe0ca01ea3c9610c842cf39a935b08f0c4e1283
Josh McHugh <joshua.mchugh01@gmail.com>
2024-04-29 23:33:14 -0400

    more examples of integer literals

--- chapter-5/integer-literals-example.scala
+++ chapter-5/integer-literals-example.scala
@@ -3,3 +3,10 @@
   val hex2 = 0x00FF
   val magic = 0xcafebabe
   val billion = 1_000_000_000
+
+  val prog = 0XCAFEBABEL
+  val tower = 35L
+  val of = 31l
+
+  val little: Short = 367
+  val littler: Byte = 38
