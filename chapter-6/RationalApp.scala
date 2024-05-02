7babd7790b027da12545713dd76dcad1fcaa2259
Josh McHugh <joshua.mchugh01@gmail.com>
2024-05-01 22:29:38 -0400

    Rational App examples of Scala objects with immutability in the design

--- chapter-6/RationalApp.scala
+++ chapter-6/RationalApp.scala
@@ -3,7 +3,13 @@ object App:
     val oneHalf = Rational(1, 2)
     val twoThirds = Rational(2, 3)
     println(s"add $oneHalf and $twoThirds = ${oneHalf + twoThirds}")
+    println(s"add $oneHalf and 2 = ${oneHalf + 2}")
+    println(s"subtract $oneHalf and $twoThirds = ${oneHalf - twoThirds}")
+    println(s"subtract $oneHalf and 2 = ${oneHalf - 2}")
     println(s"multiply $oneHalf and $twoThirds = ${oneHalf * twoThirds}")
+    println(s"multiply $oneHalf and 2 = ${oneHalf * 2}")
+    println(s"divide $oneHalf and $twoThirds = ${oneHalf / twoThirds}")
+    println(s"divide $oneHalf and 2 = ${oneHalf / 2}")
     println(s"which is greater $oneHalf or $twoThirds: ${oneHalf.max(twoThirds)}")
     println(s"whole number ${new Rational(3)}")
     println(s"normalized ${new Rational(66, 42)}")
@@ -16,22 +22,43 @@ class Rational(n: Int, d: Int):
 
   def this(n: Int) = this(n, 1)
 
-  override def toString = s"$numer/$denom"
-
-  private def gcd(a: Int, b: Int): Int =
-    if b == 0 then a else gcd(b, a % b)
-
   def + (that: Rational): Rational =
     Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
 
+  def + (i: Int): Rational =
+    Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    Rational(numer - i * denom, denom)
+
   def * (that: Rational): Rational =
     Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational =
+    Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    Rational(numer, denom * i)
 
   def lessThan(that: Rational): Boolean =
     numer * that.denom < that.numer * denom
 
   def max(that: Rational): Rational =
     if lessThan(that) then that else this
+
+  private def gcd(a: Int, b: Int): Int =
+    if b == 0 then a else gcd(b, a % b)
+
+  override def toString = s"$numer/$denom"
