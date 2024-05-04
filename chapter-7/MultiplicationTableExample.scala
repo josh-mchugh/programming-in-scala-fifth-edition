566699e291131c859dbd626f403aea71bc3ea9c7
Josh McHugh <joshua.mchugh01@gmail.com>
2024-05-03 22:41:53 -0400

    Functional example of multiplication table

--- chapter-7/MultiplicationTableExample.scala
+++ chapter-7/MultiplicationTableExample.scala
@@ -1,14 +1,16 @@
 @main def m(args: String*) =
-  var i = 1
-  while i <= 10 do
-    var j = 1
-    while j <= 10 do
-      val prod = (i * j).toString
-      var k = prod.length
-      while k < 4 do
-        print(" ")
-        k += 1
-      print(prod)
-      j += 1
-    println()
-    i += 1
+  println(multiTable())
+
+def makeRowSeq(row: Int) =
+  for col <- 1 to 10 yield
+    val prod = (row * col).toString
+    val padding = " " * (4 - prod.length)
+    padding + prod
+
+def makeRow(row: Int) = makeRowSeq(row).mkString
+
+def multiTable() =
+  val tableSeq =
+    for row <- 1 to 10
+    yield makeRow(row)
+  tableSeq.mkString("\n")
