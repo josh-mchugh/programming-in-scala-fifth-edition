cfbba05959a2d2be58a29fb9d811b8954af92a34
Josh McHugh <joshua.mchugh01@gmail.com>
2024-05-14 23:29:46 -0400

    Example of constructing Lists

---
+++ chapter-14/ConstructingListExample.scala
@@ -0,0 +1,7 @@
+@main def m(args: String*) =
+  val fruit = "apples" :: "oranges" :: "pears" :: Nil
+  val nums = 1 :: 2 :: 3 :: 4 :: Nil
+  val diag3 = (1 :: 0 :: 0 :: Nil) ::
+              (0 :: 1 :: 0 :: Nil) ::
+              (0 :: 0 :: 1 :: Nil) :: Nil
+  val empty = Nil
