0b14e736ed151cfefbe9827cd9c747b06c006e86
Josh McHugh <joshua.mchugh01@gmail.com>
2024-05-29 20:30:51 -0400

    Examples of Givens imports

--- chapter-21/SimpleGivensExample.scala
+++ chapter-21/SimpleGivensExample.scala
@@ -1,10 +1,15 @@
 import JillsPrefs.{prompt as jillPrompt}
-import JoesPrefs.{prompt, drink}
+import JoesPrefs.{prompt as tomPrompt, drink as tomDrink}
+import TomsPrefs.prompt // imports prompt
+import TomsPrefs.given // imports prompt, drink, prefPromptOrd, and prefDrinkOrd
+import TomsPrefs.{given PreferredDrink} // imports drink because it is given of type PreferredDrink
+import TomsPrefs.{given Ordering[PreferredPrompt], given Ordering[PreferredDrink]} // imports prefPromptOrd and prefDrinkOrd
+import TomsPrefs.{given Ordering[?]} // imports prefPromptOrd and preDrinkOrd
 
 @main def m(args: String*) =
   println("Simple Givens")
   Greeter.greet("Jill")(using jillPrompt)
-  Greeter.greet("Joe")(using prompt, drink)
+  Greeter.greet("Joe")(using tomPrompt, tomDrink)
 
 class PreferredPrompt(val preference: String)
 class PreferredDrink(val preference: String)
@@ -22,4 +27,20 @@ object JillsPrefs:
 object JoesPrefs:
   given prompt: PreferredPrompt = PreferredPrompt("relax> ")
   given drink: PreferredDrink = PreferredDrink("tea")
+
+object TomsPrefs:
+  val favoriteColor = "blue"
+  def favoriteFood = "steak"
+
+  given prompt: PreferredPrompt = PreferredPrompt("enjoy>")
+  given drink: PreferredDrink = PreferredDrink("red whine")
+  given prefPromptOrd: Ordering[PreferredPrompt] with
+    def compare(x: PreferredPrompt, y: PreferredPrompt) =
+      x.preference.compareTo(y.preference)
+  given prefDrinkOrd: Ordering[PreferredDrink] with
+    def compare(x: PreferredDrink, y: PreferredDrink) =
+      x.preference.compareTo(y.preference)
+
+
+
 
